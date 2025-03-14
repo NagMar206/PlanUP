5ae9c5fa9bcdf766cbebf9cfb5163e724dfae221
NagMar206 <nagmar206@hengersor.hu>
2025-03-14 10:13:04 +0100

    2 Adatbázis közti váltogatás
    Kikommentelve az egyik, használható a másik

--- planup-backend/config/dbConfig.js
+++ planup-backend/config/dbConfig.js
@@ -1,11 +1,24 @@
 const mysql = require('mysql2/promise');
 
-const db = mysql.createPool({
+//PlanUP Adatbázis
+/* const db = mysql.createPool({
     host: 'localhost', // XAMPP esetén ez maradjon "localhost"
     user: 'root', // XAMPP alapértelmezett felhasználó
     password: '', // XAMPP esetén nincs jelszó, hagyd üresen
     database: 'planup', // Az adatbázis neve (ellenőrizd phpMyAdminban!)
     port: 3307, // Ha a MySQL más porton fut, módosítsd
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+}); */
+
+//Iskolai szerver
+const db = mysql.createPool({
+    host: '10.3.1.65', // XAMPP esetén ez maradjon "localhost"
+    user: 'mollev545', // XAMPP alapértelmezett felhasználó
+    password: '72576822545', // XAMPP esetén nincs jelszó, hagyd üresen
+    database: 'mollev545', // Az adatbázis neve (ellenőrizd phpMyAdminban!)
+    port: 3306, // Ha a MySQL más porton fut, módosítsd
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
