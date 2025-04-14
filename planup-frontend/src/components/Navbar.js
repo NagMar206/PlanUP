53f6af73b9e8179fcdd8f371eca82e1a6ab9ed01
NagMar206 <nagmar206@hengersor.hu>
2025-04-14 12:24:08 +0200

    PlanUP alsószöveg
    Splash menu text type shi, még lehetne variálni. (also előző kommit nem mükszik)

--- planup-frontend/src/components/Navbar.js
+++ planup-frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect  } from "react";
 import { Link } from "react-router-dom";
 import "../Style/Navbar.css";
 import loginImage from "../images/login.jpg";
@@ -6,15 +6,52 @@ import logoutImage from "../images/logout.jpg";
 import { TbSwipe } from "react-icons/tb";
 import { FaUserPlus, FaSignInAlt, FaHeart } from "react-icons/fa";
 import { MdMeetingRoom } from "react-icons/md";
+import axios from "axios";
+
 
 function Navbar({ user }) {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [welcomeMessage, setWelcomeMessage] = useState("");
+
+ // Előre betáplált üdvözlő szövegek
+ const welcomeMessages = [
+  "Hagyd ránk a tervezést!",
+  "Fedezd fel a legjobb programokat!",
+  "Kalandra fel, jó szórakozást!"
+];
+
+
+  useEffect(() => {
+    if (!user) return;
+    // Felhasználói név lekérése
+    axios
+      .get(`http://localhost:3001/profile/${user}`, { withCredentials: true })
+      .then((response) => {
+        setUsername(response.data.username);
+      })
+      .catch((error) => {
+        console.error("Hiba a felhasználói név lekérésekor:", error);
+      });
+  }, [user]);
+
+  useEffect(() => {
+    // Véletlenszerű üdvözlő szöveg kiválasztása
+    const randomMessage =
+      welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)];
+    setWelcomeMessage(`${randomMessage}`);
+  }, [username]); // Az üdvözlő szöveg frissül, ha a felhasználó neve változik
 
   return (
     <nav className="navbar">
+      <div className="navbar-left">
       <Link className="navbar-title" to="/">
         PlanUp
       </Link>
+        {username && (
+        <span className="navbar-welcome">{welcomeMessage}</span>
+        )}
+      </div>
 
       {/* Hamburger ikon */}
       <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
